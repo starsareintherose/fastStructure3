f4ae6c3831609ce7d1bbc4946b77e57e6dcf4940
Guoyi Zhang <kuoi@bioarchlinux.org>
2025-09-24 15:03:39 +1000

    Update import statement for admixprop module

--- vars/allelefreq.pyx
+++ vars/allelefreq.pyx
@@ -1,7 +1,7 @@
 
 import numpy as np
 cimport numpy as np
-cimport admixprop as ap
+cimport vars.admixprop as ap
 from cpython cimport bool
 from functools import reduce
 from scipy.special import digamma, gammaln, polygamma
