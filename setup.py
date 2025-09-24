0d25cf80bf5cde4e484086858fb8c2c214eef9b3
Guoyi Zhang <kuoi@bioarchlinux.org>
2025-09-24 14:54:16 +1000

    Refactor setup.py include_dirs and py_modules order

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ from Cython.Build import cythonize
 import numpy
 
 ext_modules = [
-    Extension("fastStructure", ["fastStructure.pyx"], include_dirs=[numpy.get_include(), '.', 'vars/']),
-    Extension("parse_bed", ["parse_bed.pyx"], include_dirs=[numpy.get_include(), '.']),
-    Extension("parse_str", ["parse_str.pyx"], include_dirs=[numpy.get_include(), '.']),
+    Extension("fastStructure", ["fastStructure.pyx"], include_dirs=[numpy.get_include(), "vars", "."]),
+    Extension("parse_bed", ["parse_bed.pyx"], include_dirs=[numpy.get_include(), "."]),
+    Extension("parse_str", ["parse_str.pyx"], include_dirs=[numpy.get_include(), "."]),
 ]
 
 setup(
     name="fastStructure3",
     version="0.0.1",
     ext_modules=cythonize(ext_modules),
-    py_modules=["structure", "chooseK", "distruct"],  # 如有这些文件
-    include_dirs=[numpy.get_include(), '.', 'vars/'],
+    py_modules=["chooseK", "distruct", "structure"],
     license="MIT",
-    description="Python3 compatible update of Anil Raj's fastStructure"
+    description="Python3 compatible update of Anil Raj's fastStructure",
 )
