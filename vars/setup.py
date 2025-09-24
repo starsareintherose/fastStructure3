80f597b950fd45009653be915da16467a6430a0d
Guoyi Zhang <kuoi@bioarchlinux.org>
2025-09-24 14:57:13 +1000

    Update setup.py

--- vars/setup.py
+++ vars/setup.py
@@ -3,17 +3,23 @@ from Cython.Build import cythonize
 import numpy
 
 ext_modules = [
-    Extension("vars.admixprop", ["admixprop.pyx", "C_admixprop.c"], include_dirs=[numpy.get_include(), "."]),
-    Extension("vars.allelefreq", ["allelefreq.pyx", "C_allelefreq.c"], 
-              libraries=["gsl", "gslcblas"], extra_compile_args=["-O3"], include_dirs=[numpy.get_include(), "."]),
-    Extension("vars.marglikehood", ["marglikehood.pyx", "C_marglikehood.c"], include_dirs=[numpy.get_include(), "."]),
-    Extension("vars.utils", ["utils.pyx"], include_dirs=[numpy.get_include(), "."]),
+    Extension("fastStructure", ["fastStructure.pyx"], include_dirs=[numpy.get_include(), "vars", "."]),
+    Extension("parse_bed", ["parse_bed.pyx"], include_dirs=[numpy.get_include(), "."]),
+    Extension("parse_str", ["parse_str.pyx"], include_dirs=[numpy.get_include(), "."]),
+    Extension("vars.admixprop", ["vars/admixprop.pyx", "vars/C_admixprop.c"], include_dirs=[numpy.get_include(), "vars"]),
+    Extension("vars.allelefreq", ["vars/allelefreq.pyx", "vars/C_allelefreq.c"], 
+              libraries=["gsl", "gslcblas"], extra_compile_args=["-O3"], include_dirs=[numpy.get_include(), "vars"]),
+    Extension("vars.marglikehood", ["vars/marglikehood.pyx", "vars/C_marglikehood.c"], include_dirs=[numpy.get_include(), "vars"]),
+    Extension("vars.utils", ["vars/utils.pyx"], include_dirs=[numpy.get_include(), "vars"]),
 ]
 
 setup(
-    name="variables",
-    version="1.0",
+    name="fastStructure3",
+    version="0.0.1",
     ext_modules=cythonize(ext_modules),
-    packages=["vars"],
-    include_dirs=[numpy.get_include(), "."],
+    py_modules=["chooseK", "distruct", "structure"],
+    packages=["vars"], 
+    include_dirs=[numpy.get_include(), "vars", "."],
+    license="MIT",
+    description="Python3 compatible update of Anil Raj's fastStructure",
 )
