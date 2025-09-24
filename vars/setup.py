885440dbf49fcef4c188b8698e35636303eb5413
Guoyi Zhang <kuoi@bioarchlinux.org>
2025-09-24 14:54:38 +1000

    Fix formatting and ensure proper extension setup

--- vars/setup.py
+++ vars/setup.py
@@ -3,11 +3,11 @@ from Cython.Build import cythonize
 import numpy
 
 ext_modules = [
-    Extension("vars.admixprop", ["admixprop.pyx", "C_admixprop.c"], include_dirs=[numpy.get_include(), '.']),
+    Extension("vars.admixprop", ["admixprop.pyx", "C_admixprop.c"], include_dirs=[numpy.get_include(), "."]),
     Extension("vars.allelefreq", ["allelefreq.pyx", "C_allelefreq.c"], 
-              libraries=["gsl", "gslcblas"], extra_compile_args=["-O3"], include_dirs=[numpy.get_include(), '.']),
-    Extension("vars.marglikehood", ["marglikehood.pyx", "C_marglikehood.c"], include_dirs=[numpy.get_include(), '.']),
-    Extension("vars.utils", ["utils.pyx"], include_dirs=[numpy.get_include(), '.']),
+              libraries=["gsl", "gslcblas"], extra_compile_args=["-O3"], include_dirs=[numpy.get_include(), "."]),
+    Extension("vars.marglikehood", ["marglikehood.pyx", "C_marglikehood.c"], include_dirs=[numpy.get_include(), "."]),
+    Extension("vars.utils", ["utils.pyx"], include_dirs=[numpy.get_include(), "."]),
 ]
 
 setup(
@@ -15,5 +15,5 @@ setup(
     version="1.0",
     ext_modules=cythonize(ext_modules),
     packages=["vars"],
-    include_dirs=[numpy.get_include(), '.'],
+    include_dirs=[numpy.get_include(), "."],
 )
